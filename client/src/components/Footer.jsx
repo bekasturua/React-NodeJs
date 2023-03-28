8eb3c21d3ba9b008df892776b0f88b570c1585dc
Beka Sturua <beqasturua3@gmail.com>
2023-03-29 00:41:44 +0400

    add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Logo from "../img/logo.webp"
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+
+        <footer>
+            <img src={Logo} alt="" />
+            <span>Made with  and <b>React.js</b>.</span>
+        </footer>
     )
 }
 
