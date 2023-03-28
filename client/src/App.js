d308bc7080107737eddffd60ae8cf48f11f66157
Beka Sturua <beqasturua3@gmail.com>
2023-03-28 19:16:24 +0400

    add routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,76 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
