4b63de35840a39b9a4a1d4dac86c6b332cc1d86b
Beka Sturua <beqasturua3@gmail.com>
2023-04-01 13:27:07 +0400

    add single page html and styles

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Edit from "../img/edit.png"
+import Delete from "../img/delete.png"
+import Manu from "../components/Menu"
+import { Link } from "react-router-dom";
+
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className='single'>
+            <div className="content">
+                <img src="https://viragonline.cdn.shoprenter.hu/custom/viragonline/image/data/oltott_kaktusz_9cm-s_cserepben_sarg.jpg?lastmod=1526665352.1515754753" alt="" />
+                <div className="user">
+                    <img src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/315626011_5495793820469604_1420049836398030074_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NE7lsHlhfCgAX_tYMCZ&_nc_oc=AQlslJ1VGcgZFT96awwhgZ-OTCkeFTrGUduqUaPm0GTlsgoY_3TozropVBV69COwx30&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfAIVH2j0A5BBzqeIIv74Fd67t6JDqh2b0v6vP03alP2dA&oe=6437F55B" alt="" />
+                    <div className="info">
+                        <span>Beka</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                    <div className="edit">
+                        <Link to={`/write?edit=2`}>
+                            <img src={Edit} alt="" />
+                        </Link>
+                        <img src={Delete} alt="" />
+                    </div>
+                </div>
+                <h1>loren</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis? ipsum dolor, sit amet consectetur adipisicing elit. Eum, ipsum! Voluptate, earum neque! Quidem doloremque ad, obcaecati officia a quae corrupti nemo perferendis excepturi placeat, totam autem quia? Quia, perferendis?</p>
+            </div>
+            <Manu />
+        </div>
     )
 }
 
